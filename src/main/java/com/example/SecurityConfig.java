bb359d9fb10ba820a2d5032ef0a1a72865d651d4
kumaotto <won.artoiler09@icloud.com>
2021-11-05 14:46:48 +0900

    3.5.3 Configure Authentication and Authorization by JavaConfig

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,5 +1,32 @@
 package com.example;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
+// spring securityの基本的な設定をする
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    // 静的ソースのセキュリティ設定を無視させる
+    web.ignoring().antMatchers("/webjars/**", "/css/**");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/loginForm").permitAll().anyRequest().authenticated().and().formLogin()
+        .loginProcessingUrl("/login").loginPage("/loginForm").failureUrl("/loginForm?error")
+        .defaultSuccessUrl("/customers", true).usernameParameter("username").passwordParameter("password").and()
+        .logout().logoutSuccessUrl("/loginForm");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new Pbkdf2PasswordEncoder();
+  }
 }
