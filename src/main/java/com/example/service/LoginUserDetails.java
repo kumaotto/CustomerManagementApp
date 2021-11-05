1cbb45ef9e928b16e2c9b3306d5c162844ce1629
kumaotto <won.artoiler09@icloud.com>
2021-11-05 14:25:02 +0900

    3.5.2 Add authenticated user

--- src/main/java/com/example/service/LoginUserDetails.java
+++ src/main/java/com/example/service/LoginUserDetails.java
@@ -1,5 +1,15 @@
 package com.example.service;
 
-public class LoginUserDetails {
+import com.example.domain.User;
+import org.springframework.security.core.authority.AuthorityUtils;
+import lombok.Data;
 
+@Data
+public class LoginUserDetails extends org.springframework.security.core.userdetails.User {
+  private final User user;
+
+  public LoginUserDetails(User user) {
+    super(user.getUserName(), user.getEncodedPassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
+    this.user = user;
+  }
 }
