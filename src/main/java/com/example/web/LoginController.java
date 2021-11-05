e216db654ace31bcd72232dfea311fdb1ff7460d
kumaotto <won.artoiler09@icloud.com>
2021-11-05 15:12:23 +0900

    3.5.5 Add Pages for using username

--- src/main/java/com/example/web/LoginController.java
+++ src/main/java/com/example/web/LoginController.java
@@ -1,5 +1,12 @@
 package com.example.web;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LoginController {
+  @GetMapping(path = "loginForm")
+  String loginForm() {
+    return "loginForm";
+  }
 }
